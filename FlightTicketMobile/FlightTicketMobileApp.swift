370b72d462cc1ab1782966e4d0c0ea1fa572fa7d
HuseyinBugraEKEN <bugraeken46@gmail.com>
2024-08-16 16:02:04 +0300

    Initial Commit

---
+++ FlightTicketMobile/FlightTicketMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTicketMobileApp.swift
+//  FlightTicketMobile
+//
+//  Created by Buğra EKEN on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightTicketMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
